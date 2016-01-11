2d645cb4252f33985402d8b617e1e9edd5a5468b
MilesJPool <MilesJPool@gmail.com>
2016-01-11 16:14:40 +0000

    Initial

---
+++ stringcalculator.Test/StringCalculatorTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace Stringcalculator.Test
+{
+    public class StringCalculatorTest
+    {
+        [TestFixture]
+        public class TheAddMethodShould
+        {
+            [Test]
+            public void Returns0WhenGivenAnEmptyString()
+            {
+                Assert.That(StringCalculator.Add(string.Empty), Is.EqualTo(0));
+            }
+
+            [TestCase("1", 1)]
+            [TestCase("1,2", 3)]
+            [TestCase("1,2,3,4", 10)]
+            public void SumCommaSeparatedNumbers(string csv, int sum)
+            {
+                Assert.That(StringCalculator.Add(csv), Is.EqualTo(sum));
+            }
+
+            [Test]
+            public void SumNumbersSeparatedByNewLines()
+            {
+                Assert.That(StringCalculator.Add("1\n2,3"), Is.EqualTo(6));
+            }
+
+            [Test]
+            public void SumNumbersSeparatedByCustomDelimiter()
+            {
+                Assert.That(StringCalculator.Add("//;\n1;2"), Is.EqualTo(3));
+            }
+
+            [Test]
+            public void ReportIfAnyNegativeNumbersAreInTheCsv()
+            {
+                Assert.That(() => StringCalculator.Add("1,-2,-4"),
+                    Throws.ArgumentException.With.Message.EqualTo("negatives not allowed: -2, -4"));
+            }
+        }
+    }
+}
---
+++ stringcalculator/StringCalculator.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stringcalculator
+{
+    public class StringCalculator
+    {
+        public static long Add(string csvText)
+        {
+            return IsEmpty(csvText)
+                ? 0
+                : TheSumOfTheElementsIn(csvText);
+        }
+
+        private static bool IsEmpty(string csvText)
+        {
+            return csvText == string.Empty;
+        }
+
+        private static long TheSumOfTheElementsIn(string csvText)
+        {
+            var elements = TheIndividualElementsOfThe(csvText);
+            ValidateThatThereAreNoNegativesIn(elements);
+            return elements.Sum();
+        }
+
+        private static IList<long> TheIndividualElementsOfThe(string csvText)
+        {
+            return Csv.Read(csvText).GetIndividualElements().ToList();
+        }
+
+        private static void ValidateThatThereAreNoNegativesIn(IEnumerable<long> elements)
+        {
+            var negatives = elements.Where(IsNegative).ToList();
+
+            if (negatives.Count() != 0)
+            {
+                throw new ArgumentException(string.Format("negatives not allowed: {0}", string.Join(", ", negatives)));
+            }
+        }
+
+        private static bool IsNegative(long elem)
+        {
+            return elem < 0;
+        }
+    }
+}
